e7f2d311b7fab762f96e0cf1df81b895b5169ef8
Alzemiro Iago de M. Thomaz <iagomoraest@gmail.com>
2022-02-14 22:49:50 -0300

    Read Me

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,32 +1,19 @@
 import { Jai } from '../index';
 
 const api = new Jai({
-  access_token: 'ad42551e7312487a90c8b3be539caec1',
-});
-
-describe('Test for service status', () => {
-  it('should return the status from JAI', async () => {
-    const response = await api.status();
-    expect(response.General).toBeTruthy();
-  });
+  access_token: '',
 });
 
 describe('Test for getAuth', () => {
   it('should return the auth from JAI', async () => {
     const response = await api.getAuthKey({
-      email: 'iagomoraest@gmail.com',
-      firstName: 'Alzemiro',
-      lastName: 'Thomaz',
+      email: 'teste@teste.com',
+      firstName: 'Teste',
+      lastName: 'Teste',
       company: '',
     });
     expect(response.status).toBeLessThan(300)
   })
 })
 
-describe('Test for similar', () => {
-  it('should return similarity result', async () => {
-    const data = [1000];
-    const response = await api.similar('cc_fraud_supervised', data);
-    expect(response.similarity).toBeTruthy();
-  });
-});
+
